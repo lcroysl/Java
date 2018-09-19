eb30639385f45ddd51ff2c4d19b71095ae5e46c8
lcroysl <huanjoe@live.se>
2018-09-19 15:58:03 +0200

    Uppgift 1,2

---
+++ Test folder/src/test/loop.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Scanner;
+
+public class loop {
+	public static void main(String[] args) {
+		Scanner lol = new Scanner(System.in);
+		
+		for (int i = 1; i < 101; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ Test folder/src/test/loop1.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class loop1 {
+	public static void main(String[] args) {
+		Scanner lol = new Scanner(System.in);
+		
+		for (int i = 25; i < 51; i++) {
+			System.out.println(i);
+			
+		}
+		
+		
+	}
+
+}
