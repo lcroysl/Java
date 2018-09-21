70d38ee945c1f6ea88a7640f27721c1a4239095f
lcroysl <huanjoe@live.se>
2018-09-21 14:52:55 +0200

    Timeloop

---
+++ Uppgift_v38/src/Uppgifter/Timeloop.java
@@ -0,0 +1,18 @@
+package Uppgifter;
+
+import java.util.Scanner;
+
+public class Timeloop {
+	public static void main(String[] args) {
+		
+		Scanner inp = new Scanner(System.in);
+				
+				int N = inp.nextInt();
+				
+				for (int i = 1; i < N+1; i++) {
+					System.out.println(i + " Abracadabra");
+					
+				}
+	}
+
+}
